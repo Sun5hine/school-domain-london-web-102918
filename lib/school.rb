63eefaaabeb4ce810db84e1c3ec634e4d0773477
flatironschool <flatironschool@flatirooolsair2.home>
2018-11-11 22:32:07 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+require 'pry'
+
+class School
+
+@@roster = []
+
+def initialize(name)
+  @name = name
+  @@roster << self
+end
+
+def self.all
+  @@roster
+end
+
+def sort
+  roster.sort
+end
+
+end
+
+school1 = School.new("tara")
+school2 = School.new("asda")
+school3 = School.new("ghja")
+school4 = School.new("ytfa")
+
+pry.start
